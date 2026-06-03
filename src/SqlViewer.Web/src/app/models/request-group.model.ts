711d820d2d57657d13ce1428a131bfd5dfb10fd5
Krzysztof Koźlik (KLF) <KLF@kmd.dk>
2026-06-03 21:10:03 +0200

    Aligned data in rows

--- src/SqlViewer.Web/src/app/models/request-group.model.ts
+++ src/SqlViewer.Web/src/app/models/request-group.model.ts
@@ -2,8 +2,8 @@ import { SqlCommandEvent } from './sql-command-event.model';
 
 export interface RequestGroup {
   spanId: string;
+  traceId: string;
   url: string;
-  methodName: string;
   commands: SqlCommandEvent[];
   totalDurationUs: number;
   capturedAt: string;
